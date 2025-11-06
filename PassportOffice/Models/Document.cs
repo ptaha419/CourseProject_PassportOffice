133e4a0bb3294e30e59c6892fde9cd08d94699e3
ptaha <ptaha@PK>
2025-11-06 09:13:08 +0300

    Создан класс Document (Документ) и добавлены поля для него

---
+++ PassportOffice/Models/Document.cs
@@ -0,0 +1,11 @@
+﻿namespace PassportOffice.Models
+{
+    public class Document
+    {
+        private int id;
+        private string number;
+        protected string authority; 
+        protected DateOnly startDate; 
+        protected DateOnly endDate; 
+    }
+}
