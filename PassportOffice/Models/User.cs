d8dfde617994a2b6f96673a6078a551b06932214
ptaha <ptaha@PK>
2025-11-06 09:09:42 +0300

    Создан класс User (Пользователь) и добавлены поля для него

---
+++ PassportOffice/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace PassportOffice.Models
+{
+    public class User
+    {
+        private int id;
+        private string login; 
+        private string password;
+        public string surname;
+        public string middleName;
+        public string name;
+        public DateOnly birthDate;
+        public string gender;
+        protected string phoneNumber; 
+        protected string email; 
+    }
+}
