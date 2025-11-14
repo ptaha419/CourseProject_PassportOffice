8e382e5f7858d181454b5a8132cb3918246e9738
ptaha <ptaha@PK>
2025-11-14 22:01:36 +0300

    Изменён класс Application: добавлены элементы перечисления TypeOfApplication и поля для конструктора

--- PassportOffice/Models/Application.cs
+++ PassportOffice/Models/Application.cs
@@ -6,25 +6,49 @@ namespace PassportOffice.Models
     {
         private int id;
         private Applicant applicant;
-        public enum typeOfApplication { };
-        public DateOnly startDate;
-        public DateOnly endDate;
-        public string status;
-        public string description;
-        private List<Document> attachedDocs;
-        private Employee employee;
-        private string applicationReview;
+        public enum TypeOfApplication 
+        {
+            IssuingAndReplacingPassport, // Выдача и смена паспорта
+            ForeignPassportProcessing, // Оформление загранпаспорта
+            IssuanceResidencePermit, // Выдача вида на жительство 
+            RegistrationByTemporaryResidence, // Регистрация по месту пребывания 
+            RegistrationByPermanentResidence, // Регистрация по месту жительства 
+            Deregistration // Снятие с регистрационного учёта
+        }
 
-        public Application(int id, Applicant applicant, Employee employee)
+        public TypeOfApplication typeOfApplication { get; set; }
+        public DateOnly StartDate;
+        public DateOnly EndDate;
+        public string Status;
+        public string Description;
+        private List<Document> AttachedDocs;
+        private Employee employee;
+        private string ApplicationReview;
+
+        public Application(int id, Applicant applicant, TypeOfApplication typeOfApp,
+            DateOnly startDate, DateOnly endDate, string status, string description,
+            List<Document> attachedDocs, Employee employee, string applicationReview)
         {
             this.id = id;
-            this.applicant = applicant;
-            this.employee = employee;
+            this.applicant = applicant; 
+            this.typeOfApplication = typeOfApplication; 
+            this.StartDate = startDate; 
+            this.EndDate = endDate;
+            this.Status = status; 
+            this.Description = description; 
+            this.AttachedDocs = attachedDocs; 
+            this.employee = employee; 
+            this.ApplicationReview = applicationReview; 
         }
 
         public int GetId()
         {
             return id;
+        }
+
+        public void ChangeApplicationStatus(string NewStatus)
+        {
+            Status = NewStatus;
         }
     }
 }
