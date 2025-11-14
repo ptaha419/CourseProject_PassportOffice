e8682ca02fc232a18de95617b2396a673f81a8ba
ptaha <ptaha@PK>
2025-11-14 11:25:56 +0300

    Изменён код класса Document: добавлены методы GetTypeOfDocument(), ToString(), GetDocumentId(), IsValid()

--- PassportOffice/Models/Document.cs
+++ PassportOffice/Models/Document.cs
@@ -1,13 +1,53 @@
-﻿namespace PassportOffice.Models
+﻿using static PassportOffice.Models.Document;
+using System.Xml.Linq;
+
+namespace PassportOffice.Models
 {
     public class Document
     {
-        private int id;
-        public enum typeOfDocument { }; 
-        private string number; 
-        protected string authority; 
-        protected DateOnly startDate; 
-        protected DateOnly endDate;
-        protected Applicant applicant;
+        private readonly int _id;
+        public enum TypeOfDocument
+        {
+            Passport,
+            BirthCertificate
+        }
+        private readonly TypeOfDocument _documentType;
+        private readonly int _number;
+        protected string Authority;
+        protected DateTime StartDate;
+        protected DateTime EndDate;
+
+        public Document(int id, TypeOfDocument docType, int number, string auth,
+                        DateTime start, DateTime end)
+        {
+            _id = id;
+            _documentType = docType;
+            _number = number;
+            Authority = auth;
+            StartDate = start;
+            EndDate = end;
+        }
+
+        public TypeOfDocument GetTypeOfDocument()
+        {
+            return _documentType;
+        }
+
+        public override string ToString()
+        {
+            return $"Тип документа: {_documentType.ToString()}";
+        }
+
+        public int GetDocumentId(int _id) 
+        { 
+            return (_id) ;
+        }
+
+        public bool IsValid()
+        {
+            var today = DateTime.Today;
+            return EndDate >= today;
+        }
     }
 }
+
