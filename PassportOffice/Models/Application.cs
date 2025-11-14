d798cbd54a32545f6ceb1581e1e4caa69c1c244e
ptaha <ptaha@PK>
2025-11-14 12:14:29 +0300

    Изменён код класса Notification: добавлен метод SendNotification(). Изменён код класса Application: добавлен конструктор Application() и геттер GetId()

--- PassportOffice/Models/Application.cs
+++ PassportOffice/Models/Application.cs
@@ -1,16 +1,30 @@
-﻿namespace PassportOffice.Models
+﻿using System.Security.Cryptography;
+
+namespace PassportOffice.Models
 {
     public class Application
     {
         private int id;
-        private Applicant applicantId;
+        private Applicant applicant;
         public enum typeOfApplication { };
-        public DateOnly startDate; 
+        public DateOnly startDate;
         public DateOnly endDate;
         public string status;
-        public string description; 
+        public string description;
         private List<Document> attachedDocs;
         private Employee employee;
-        private string applicationReview; 
+        private string applicationReview;
+
+        public Application(int id, Applicant applicant, Employee employee)
+        {
+            this.id = id;
+            this.applicant = applicant;
+            this.employee = employee;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
     }
 }
--- PassportOffice/Models/Notification.cs
+++ PassportOffice/Models/Notification.cs
@@ -1,11 +1,30 @@
-﻿namespace PassportOffice.Models
+﻿using System.Security.Cryptography;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PassportOffice.Models
 {
     public class Notification
     {
-        private int id; 
-        private string title; 
-        private string text; 
-        private Applicant applicant; 
-        private Employee employee; 
+        private int id;
+        private string Title;
+        private string Text;
+        private Applicant applicant;
+        private Employee employee;
+        private Application application;
+
+        public void SendNotification(string Title, string Text, 
+            Applicant applicant, Employee employee, Application application) 
+        {
+            this.Title = Title;
+            this.Text = Text;
+            this.applicant = applicant;
+            this.employee = employee;
+            this.application = application;
+
+            Console.WriteLine($"Отправлено уведомление № {id} по заявлению № {application.GetId()}");
+            Console.WriteLine($"Заявитель: {applicant.name}, Сотрудник: {employee.name}");
+            Console.WriteLine($"Заголовок: {Title}\nСообщение: {Text}");
+        }
     }
 }
