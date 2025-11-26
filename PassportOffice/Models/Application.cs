1049d53149d31271a8c2694c860e720525155894
ptaha <ptaha@email.ru>
2025-11-26 21:46:48 +0300

    Созданы вьюшки UserForms, RegistrationForm, LoginForm и контроллер UserController

--- PassportOffice/Models/Application.cs
+++ PassportOffice/Models/Application.cs
@@ -45,21 +45,23 @@ namespace PassportOffice.Models
         private Employee employee;
         private string ApplicationReview;
 
-        public Application(int id, Applicant applicant, TypeOfApplication typeOfApp,
-            DateOnly startDate, DateOnly endDate, string status, string description,
-            List<Document> attachedDocs, Employee employee, string applicationReview)
-        {
-            this.id = id;
-            this.applicant = applicant; 
-            this.typeOfApplication = typeOfApplication; 
-            this.StartDate = startDate; 
-            this.EndDate = endDate;
-            this.Status = status; 
-            this.Description = description; 
-            this.AttachedDocs = attachedDocs; 
-            this.employee = employee; 
-            this.ApplicationReview = applicationReview; 
-        }
+        public Application() {}
+
+        //public Application(int id, Applicant applicant, TypeOfApplication typeOfApp,
+        //    DateOnly startDate, DateOnly endDate, string status, string description,
+        //    List<Document> attachedDocs, Employee employee, string applicationReview)
+        //{
+        //    this.id = id;
+        //    this.applicant = applicant;
+        //    this.typeOfApplication = typeOfApplication;
+        //    this.StartDate = startDate;
+        //    this.EndDate = endDate;
+        //    this.Status = status;
+        //    this.Description = description;
+        //    this.AttachedDocs = attachedDocs;
+        //    this.employee = employee;
+        //    this.ApplicationReview = applicationReview;
+        //}
 
         public int GetId()
         {
