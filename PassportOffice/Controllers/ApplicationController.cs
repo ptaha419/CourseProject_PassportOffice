4f3c538b9952b14d13925b6fa061a52887718ace
ptaha <ptaha@email.ru>
2025-11-26 19:50:18 +0300

    Создана вьюшка AllApplications и контроллер ApplicationController. В шапку веб-приложения добавлены ссылки на вьюшки ApplicationForm и AllApplications

---
+++ PassportOffice/Controllers/ApplicationController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PassportOffice.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: ApplicationController   
+        public IActionResult Applicationform()
+        {
+            return View("ApplicationForm");
+        }
+
+        public IActionResult AllApplications()
+        {
+            return View("AllApplications");
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ApplicationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ApplicationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ApplicationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ApplicationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ApplicationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ApplicationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ApplicationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
