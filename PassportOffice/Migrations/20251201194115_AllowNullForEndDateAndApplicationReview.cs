f250f3acf19bf809f39038058704e572285079bc
ptaha <ptaha@email.ru>
2025-12-01 22:47:06 +0300

    Поля EndDate и ApplicationReview класса Application стали иметь значение null по умолчанию

---
+++ PassportOffice/Migrations/20251201194115_AllowNullForEndDateAndApplicationReview.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PassportOffice.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullForEndDateAndApplicationReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationReview",
+                table: "Applications",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Applications",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValueSql: "",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApplicationReview",
+                table: "Applications",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+    }
+}
